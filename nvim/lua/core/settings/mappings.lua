2e72d73285cb50c7d18b3a29dbee7e89a70abca0
Daniel L <khleungaw@connect.ust.hk>
2024-04-04 09:29:23 +0000

    feat: mappings for navigation

--- nvim/lua/core/settings/mappings.lua
+++ nvim/lua/core/settings/mappings.lua
@@ -5,6 +5,7 @@ vim.keymap.set("i", "<C-z>", vim.cmd.u, { desc = "Undo" })
 vim.keymap.set("n", "<C-z>", vim.cmd.u, { desc = "Undo" })
 
 vim.keymap.set("n", "<C-s>", vim.cmd.w, { desc = "Save" })
+vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>", { desc = "Save", silent = true })
 vim.keymap.set("n", "<leader>qq", vim.cmd.q, { desc = "[Q]uit" })
 vim.keymap.set("n", "<leader>qa", vim.cmd.qa, { desc = "[Q]uit All" })
 
@@ -14,6 +15,11 @@ vim.keymap.set("n", "<Tab>", ">>_", { desc = "Incrase indent" })
 vim.keymap.set("v", "<Tab>", ">gv", { desc = "Incrase indent" })
 vim.keymap.set("n", "<S-Tab>", "<<_", { desc = "Decrease indent" })
 vim.keymap.set("v", "<S-Tab>", "<gv", { desc = "Decrease indent" })
+
+vim.keymap.set("n", "<S-Left>", "b", { desc = "Previous word" })
+vim.keymap.set("n", "<S-Right>", "w", { desc = "Next word" })
+vim.keymap.set("n", "<S-Up>", "{", { desc = "Previous paragraph" })
+vim.keymap.set("n", "<S-Down>", "}", { desc = "Next paragraph" })
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
@@ -38,3 +44,8 @@ vim.keymap.set("n", "<C-Left>", "<C-w><C-h>", { desc = "Move focus to the left w
 vim.keymap.set("n", "<C-Right>", "<C-w><C-l>", { desc = "Move focus to the right window", silent = true })
 vim.keymap.set("n", "<C-Down>", "<C-w><C-j>", { desc = "Move focus to the lower window", silent = true })
 vim.keymap.set("n", "<C-Up>", "<C-w><C-k>", { desc = "Move focus to the upper window", silent = true })
+
+vim.keymap.set("i", "<C-Left>", "<Esc><C-w><C-h>", { desc = "Move focus to the left window", silent = true })
+vim.keymap.set("i", "<C-Right>", "<Esc><C-w><C-l>", { desc = "Move focus to the right window", silent = true })
+vim.keymap.set("i", "<C-Down>", "<Esc><C-w><C-j>", { desc = "Move focus to the lower window", silent = true })
+vim.keymap.set("i", "<C-Up>", "<Esc><C-w><C-k>", { desc = "Move focus to the upper window", silent = true })
